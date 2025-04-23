ffa73a7b6377e7506b628fc1011930ef57ec03cc
CometBlazer <jinghaowang1997@gmail.com>
2025-04-23 12:25:29 -0400

    <!-- svelte-ignore a11y_label_has_associated_control -->

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,6 +53,7 @@
 		  <div class="flex items-center gap-2">
 			<!-- <ModeToggle /> -->
 			<!-- Using button instead of label, and adding tabindex for dropdown trigger -->
+			<!-- svelte-ignore a11y_consider_explicit_label -->
 			<button tabindex="0" class="btn btn-ghost btn-circle">
 			  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
